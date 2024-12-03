9e071c4b1f60ace55788ab8004451578fb8efa9d
Kristoffer Hagen <kristoffer92hagen@gmail.com>
2024-12-03 21:32:33 +0100

    Commit 1

--- UserInputValidator.py
+++ UserInputValidator.py
@@ -1,13 +1,18 @@
 class UserInputValidator:
-    
+    def __init__(self):
+        self._added_ints = []
 
-    def validate_positive_integers(list):
-        added_ints = []
+    def validate_positive_integers(self, list):
+        
         for item in list:
             if item.isdigit():
                 num = int(item)
                 if num >= 0:
-                    added_ints.append(int(item))
-    
-        return added_ints
+                    self._added_ints.append(int(item))
+        return self.list_validated()
+        
+
+    def list_validated(self):
+        print(self._added_ints)
+        
             
