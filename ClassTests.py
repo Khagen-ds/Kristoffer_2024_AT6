00ef60ebfed86d3cbbe4b6eb7d2b3e7ffa40b4ca
Kristoffer Hagen <kristoffer92hagen@gmail.com>
2024-12-03 22:16:29 +0100

    Commit 3

--- ClassTests.py
+++ ClassTests.py
@@ -6,3 +6,10 @@ test1.validate_positive_integers(["1", "567", "-32", "-15", "100", "Fredrick", "
 
 test2 = UserInputValidator()
 test2.validate_positive_integers(["-1", "-567", "32", "15", "10000", "Bob", "-8.9"])
+
+# Since I allready had called the method with lists, i will provide a 3rd test as well. 
+# This one will call all the methods in the class, printing the list twice
+
+test3 = UserInputValidator()
+test3.validate_positive_integers(["0", "55", "-2", "55.6", "-78.2", "Cody", "#¤%", "3", "77"])
+test3.list_validated()
